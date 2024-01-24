b0a332297eef11319f5715e6596766ffb9718300
adrian-steinstrasser <157542229+adrian-steinstrasser@users.noreply.github.com>
2024-01-24 14:36:49 -0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket         = "terraform-remote-state-bucket-nginx-rabbitmq"  # Nome do seu bucket
+    key            = "terraform.tfstate"              # Nome do arquivo de estado no bucket
+    region         = "us-east-1"
+    }
 }
 provider "aws" {
   profile = "default"
